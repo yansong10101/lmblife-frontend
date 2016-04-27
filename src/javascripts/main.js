d940e3296590182c4c42aabf0f35b24e13cb77b2
ypling <leonard.li.pub@gmail.com>
2016-04-26 22:43:55 -0700

    ISSUE-12 Enhance TransitionManager

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -8,11 +8,4 @@ import App from './App.js';
 import { browserHistory} from 'react-router';
 const store = configureStore();
 
-browserHistory.listen(function (location) {
-  const path = (/#(\/.*)$/.exec(location.hash) || [])[1];
-  if (path) {
-    browserHistory.replace(path);
-  }
-});
-
 ReactDOM.render(<Provider store={store}><App/></Provider>,document.getElementById('main'));
